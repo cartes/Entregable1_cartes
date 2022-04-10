8adebb5792330e1e6879633c7271a0d7d25019a2
cartes <cristiancartesa@gmail.com>
2022-04-10 13:33:21 -0400

    Creados los primeros modelos

---
+++ AppSeller/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    nombres = models.CharField(max_length=55)
+    apellidos = models.CharField(max_length=55)
+    role = models.IntegerField()
+
+class RoleUsuario(models.Model):
+    nombre = models.CharField(max_length=60)
+    role_number = models.IntegerField()
+
+class Producto(models.Model):
+    user_id = models.IntegerField()
+    titulo = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    precio = models.FloatField()
+    fecha_publicacion = models.DateField()
